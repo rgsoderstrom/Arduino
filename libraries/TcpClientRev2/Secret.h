6a8344c97913c5bf7e9eef78436ecbebf8757e15
Randy Soderstrom <rgsoderstrom@gmail.com>
2024-07-22 18:55:16 -0700

    Impound Secret.h after removing the secret parts

---
+++ libraries/TcpClientRev2/Secret.h
@@ -0,0 +1,17 @@
+
+//
+// secret.h - WIFI password
+//
+
+#ifndef SECRET_H
+#define SECRET_H
+
+//const char *SSID   = "S____________t";
+//const char *PASS   = "s___________3";
+
+const char *SSID = "DESKTOP-TG91M7V 8339";
+const char *PASS = "27}0v12R";
+
+const char *server = "RandysLG";
+//const char *server = "RandysLaptop";
+#endif
