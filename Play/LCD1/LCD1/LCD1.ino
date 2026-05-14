6f6c7ad4b50b4671dfa51689f5bbc20b5d833193
Randy Soderstrom <rgsoderstrom@gmail.com>
2026-05-13 17:07:07 -0700

    LCD demo

---
+++ Play/LCD1/LCD1/LCD1.ino
@@ -0,0 +1,12 @@
+
+// LCD1
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Play/LCD1/LCD1/ReadMe.txt
@@ -0,0 +1,4 @@
+
+Demo program at:
+
+C:\Users\rgsod\Documents\Arduino\libraries\Adafruit_RGBLCDShield\examples\HelloWorld
