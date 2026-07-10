aaa2ea6931de2d56b3d2282980a96852fb48a384
Randy Soderstrom <rgsoderstrom@gmail.com>
2026-07-10 07:26:27 -0700

    Hotspot SSID & password

--- libraries/TcpClientRev2/Secret.h
+++ libraries/TcpClientRev2/Secret.h
@@ -12,9 +12,9 @@
 const char *SSID = "DESKTOP-TG91M7V 8339";
 const char *PASS = "27}0v12R";
 
-const char *servers [] = {"RandysLG", 
-                          "RandysLaptop"};
+const char *servers [] = {"RandysLG"}; 
+                   //     "RandysLaptop"};
 						   
-const int NumberServers = sizeof (servers) / sizeof (servers [0]);
-const int FirstServer = 1; // servers [FirstServer] tried first						   
+const int NumberServers = 1; // sizeof (servers) / sizeof (servers [0]);
+const int FirstServer = 0; // servers [FirstServer] tried first						   
 #endif
